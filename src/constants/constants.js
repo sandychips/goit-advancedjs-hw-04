3c9f43e04c3f97f5973fd10759d005d7c03c4cf0
Chips <149303347+sandychips@users.noreply.github.com>
2025-01-07 14:19:30 +0200

    Add files via upload

---
+++ src/constants/constants.js
@@ -0,0 +1,21 @@
+export const refs = {
+  gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.query-form'),
+  loader: document.querySelector('.loader'),
+  loadBtn: document.querySelector('.load-btn'),
+  endWarningText: document.querySelector('.end-warning'),
+};
+
+export const PX_API_KEY = '47934856-127240b78ee1c1893a576f39c';
+
+export const STORAGE_KEY = 'query';
+
+export const simpleLightboxOptions = {
+  overlay: true,
+  overlayOpacity: 1,
+  captions: true,
+  captionPosition: 'bottom',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+};
